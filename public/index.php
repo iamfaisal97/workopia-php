9814b6fd6b6b31fc7533cc2ecf95f745017fbf71
Faisal Khan <geekfaisal01@gmail.com>
2025-04-22 17:54:12 +0530

    added namespaces, controller classes, and refracted routing

--- public/index.php
+++ public/index.php
@@ -1,8 +1,20 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 require '../helpers.php';
-require basePath('Database.php');
-require basePath('Router.php');
+
+use Framework\Router;
+
+
+// Autoloader - allows as many classses as you want so you does not need to require them individually
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
+
 
 
 
